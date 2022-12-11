1c930f0e8c5cbb4e443747a45af1619f9e88840c
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-12-11 21:02:25 +0100

    renamed dir

--- lua/openai_nvim/init.lua
+++ lua/openai_nvim/init.lua
@@ -1,5 +1,5 @@
-local M = require('nvim_openai.openai_request')
-local helpers = require('nvim_openai.helper_functions')
+local M = require('openai_nvim.openai_request')
+local helpers = require('openai_nvim.helper_functions')
 
 
 local setup = {
@@ -18,9 +18,12 @@ end
 local function get_visual_selection()
     local cs, ls = unpack(vim.fn.getpos('v'))
     local ce, le = unpack(vim.fn.getpos('.'))
-    local table = vim.api.nvim_buf_get_text(0, ls-1, cs, le, ce, {})
-    -- Sort if le < ls
-    return table
+
+    print(cs, ls)
+    print(ce, le)
+    -- local table = vim.api.nvim_buf_get_text(0, ls-1, cs, le, ce, {})
+    -- -- Sort if le < ls
+    -- return table
 end
 
 local function get_current_line()
@@ -31,10 +34,18 @@ end
 local function send_visual_selection()
     -- mian function 1 
     local table = get_visual_selection()
-    local text = helpers.table_to_string(table)
-    local answer=M.api_call(setup.api_key, setup.model, "What is an Ai?")
-    send_to_nvim({answer})
+
+    for k, v in pairs(table) do print(k, v) end
+
+
+    -- local prompt = helpers.table_to_string(table)
+
+    -- local answer=M.gpt3_api_call(setup.api_key, prompt)
+    -- local ans_tbl = helpers.string_to_table(answer, 10)
+    -- send_to_nvim(ans_tbl)
 end
+
+-- Say somthing
 
 local function send_current_line()
     -- main funciton 2 
@@ -45,14 +56,11 @@ local function send_current_line()
     send_to_nvim(ans_tbl)
 end
 
-
--- dev: Set keymaps 
-local opts = { noremap=true, silent=true }
-vim.keymap.set('v', '<leader>a', send_visual_selection, opts)
-vim.keymap.set('n', '<leader>a', send_current_line, opts)
+-- what is an AI?
 
 return {
-    send_current_line = send_current_line
+    send_current_line = send_current_line,
+    send_visual_selection = send_visual_selection
 }
 
 
