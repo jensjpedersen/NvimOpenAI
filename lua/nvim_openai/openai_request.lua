2bc86e5b3a763a0f5e3a860e0eee8a2a9186eda6
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-12-11 11:54:56 +0100

    Added gpt3_api_call

--- lua/nvim_openai/openai_request.lua
+++ lua/nvim_openai/openai_request.lua
@@ -1,8 +1,3 @@
-
-
-
-
-
 local M = {}
 
 function M.api_call(api_key, model, prompt)
@@ -21,6 +16,40 @@ function M.api_call(api_key, model, prompt)
 
     return match
 
+end
+
+function M.gpt3_api_call(api_key, prompt)
+    local command=string.format([[
+        curl -s https://api.openai.com/v1/completions \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer %s" \
+        -d '{"model": "text-davinci-003", "prompt": "%s", "temperature": 0.5, "max_tokens": 2048}'
+        ]], api_key, prompt)
+
+    local output = io.popen(command)
+    local output = output:read('*a')
+
+    -- start, stop, match = string.find(output, '"text":(.+)","index"')
+    start, stop, match = string.find(output, [["text":"[\?]?\n\n(.+)","index"]])
+
+    return match
+end
+
+function M.chat_gpt_api_call(api_key, prompt)
+    local command=string.format([[
+        curl -s https://api.openai.com/v1/images/generations \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer %s" \
+        -d '{"model": "chatbot", "prompt": "%s"}'
+        ]], api_key, prompt)
+
+    local output = io.popen(command)
+    local output = output:read('*a')
+
+    -- start, stop, match = string.find(output, '"text":(.+)","index"')
+    start, stop, match = string.find(output, [["text":"\n\n(.+)","index"]])
+
+    return match
 end
 
 
