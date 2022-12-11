0c7c6080a6073aecca441a3b4c5f7b8c2c5eca04
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-12-11 21:25:51 +0100

    Working visual line mode

--- lua/openai_nvim/init.lua
+++ lua/openai_nvim/init.lua
@@ -1,7 +1,6 @@
 local M = require('openai_nvim.openai_request')
 local helpers = require('openai_nvim.helper_functions')
 
-
 local setup = {
     -- Replace `<YOUR-API-KEY>` with your actual API key
     api_key = os.getenv('OPENAI_API_KEY'),
@@ -12,40 +11,35 @@ local setup = {
 local function send_to_nvim(table)
     local buffer = vim.api.nvim_get_current_buf()
     local r, _ = unpack(vim.api.nvim_win_get_cursor(0))
-    vim.api.nvim_buf_set_lines(buffer, r, r, false, table)
+    -- print(#table)
+    vim.api.nvim_buf_set_lines(buffer, r+#table-1, r+#table-1, false, table)
 end
 
 local function get_visual_selection()
-    local cs, ls = unpack(vim.fn.getpos('v'))
-    local ce, le = unpack(vim.fn.getpos('.'))
-
-    print(cs, ls)
-    print(ce, le)
+    -- local cs, ls = unpack(vim.fn.getpos('v'))
+    -- local ce, le = unpack(vim.fn.getpos('.'))
     -- local table = vim.api.nvim_buf_get_text(0, ls-1, cs, le, ce, {})
-    -- -- Sort if le < ls
-    -- return table
+
+    -- Only working lines
+    local cs, ls = unpack(vim.fn.getpos("'<"))
+    local ce, le = unpack(vim.fn.getpos("'>"))
+    local table = vim.api.nvim_buf_get_lines(0, ls-1, le, {})
+    return table
 end
 
 local function get_current_line()
     return vim.api.nvim_get_current_line()
 end
 
-
 local function send_visual_selection()
-    -- mian function 1 
+    -- main function 1 
     local table = get_visual_selection()
-
-    for k, v in pairs(table) do print(k, v) end
-
-
-    -- local prompt = helpers.table_to_string(table)
-
-    -- local answer=M.gpt3_api_call(setup.api_key, prompt)
-    -- local ans_tbl = helpers.string_to_table(answer, 10)
-    -- send_to_nvim(ans_tbl)
+    local prompt = helpers.table_to_string(table)
+    local answer=M.gpt3_api_call(setup.api_key, prompt)
+    local ans_tbl = helpers.string_to_table(answer, 10)
+    send_to_nvim(ans_tbl)
 end
 
--- Say somthing
 
 local function send_current_line()
     -- main funciton 2 
@@ -55,8 +49,6 @@ local function send_current_line()
     local ans_tbl = helpers.string_to_table(answer, 10)
     send_to_nvim(ans_tbl)
 end
-
--- what is an AI?
 
 return {
     send_current_line = send_current_line,
